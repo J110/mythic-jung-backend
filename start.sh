525dc83446eee10368325fc42053320c3490894d
gauravtyagi <gauravtyagi8590@gmail.com>
2026-01-21 17:31:36 +0530

    updated port number to backend

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Kill any existing Node processes on port 3000
-echo "Checking for existing processes on port 3000..."
+# Kill any existing Node processes on port 3001
+echo "Checking for existing processes on port 3001..."
 lsof -ti:3000 | xargs kill -9 2>/dev/null
 killall -9 node 2>/dev/null
 
